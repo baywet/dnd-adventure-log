92e4fee7357a2d47e9957a96eb00497c681e1106
Vincent Biret <vincentbiret@hotmail.com>
2025-09-18 09:33:00 -0400

    chore: refactors storage operations out of recordings controller
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- api/CampaignStorageService.cs
+++ api/CampaignStorageService.cs
@@ -38,7 +38,7 @@ public class CampaignStorageService
 	public async Task<string> GetCampaignTranscriptionForCharactersAsync(string campaignName, CancellationToken cancellationToken)
 	{
 		// get the first episode transcript file by oldest creation date first
-		var transcriptFile = new DirectoryInfo(RecordingOperations.GetTranscriptionsRootPath(campaignName))
+		var transcriptFile = new DirectoryInfo(GetTranscriptionsRootPath(campaignName))
 			.GetFiles("*.txt")
 			.OrderBy(static f => f.CreationTime)
 			.FirstOrDefault();
@@ -88,8 +88,78 @@ public class CampaignStorageService
 		var imageStream = File.OpenRead(imagePath);
 		return imageStream;
 	}
+
+	public async Task<string> SaveRecordingAsync(string campaignName, string recordingName, Stream recordingStream, CancellationToken cancellationToken)
+	{
+		var recordingsRootPath = GetRecordingsRootPath(campaignName);
+		if (!Directory.Exists(recordingsRootPath))
+		{
+			Directory.CreateDirectory(recordingsRootPath);
+		}
+		var recordingPath = GetRecordingPath(campaignName, recordingName);
+		using var fileStream = File.Create(recordingPath);
+		await recordingStream.CopyToAsync(fileStream, cancellationToken).ConfigureAwait(false);
+		return Path.GetFileName(recordingPath);
+	}
+	public async Task SaveTranscriptionAsync(string campaignName, string recordingName, string transcriptionContent, CancellationToken cancellationToken)
+	{
+		var transcriptionsRootPath = GetTranscriptionsRootPath(campaignName);
+		if (!Directory.Exists(transcriptionsRootPath))
+		{
+			Directory.CreateDirectory(transcriptionsRootPath);
+		}
+		var transcriptionPath = GetTranscriptionPath(campaignName, recordingName);
+		await File.WriteAllTextAsync(transcriptionPath, transcriptionContent, cancellationToken).ConfigureAwait(false);
+	}
+	public async Task<string> GetTranscriptionAsync(string campaignName, string recordingName, CancellationToken cancellationToken)
+	{
+		var transcriptionPath = GetTranscriptionPath(campaignName, recordingName);
+		if (!File.Exists(transcriptionPath))
+		{
+			return string.Empty;
+		}
+		var transcription = await File.ReadAllTextAsync(transcriptionPath, cancellationToken).ConfigureAwait(false);
+		return transcription;
+	}
+	public async Task SaveEpicMomentTaleAsync(string campaignName, string recordingName, string taleContent, CancellationToken cancellationToken)
+	{
+		var taleFile = GetEpicMomentTextPath(campaignName, recordingName);
+		var taleDirectory = GetEpicMomentsRootPath(campaignName);
+		if (!Directory.Exists(taleDirectory) && taleDirectory is not null)
+		{
+			Directory.CreateDirectory(taleDirectory);
+		}
+		await File.WriteAllTextAsync(taleFile, taleContent, cancellationToken).ConfigureAwait(false);
+	}
+	public async Task SaveEpicMomentVideoAsync(string campaignName, string recordingName, Stream videoStream, CancellationToken cancellationToken)
+	{
+		var epicMomentVideoPath = GetEpicMomentVideoPath(campaignName, recordingName);
+		using var videoFile = File.Create(epicMomentVideoPath);
+		await videoStream.CopyToAsync(videoFile, cancellationToken).ConfigureAwait(false);
+	}
+	public string[] GetRecordings(string campaignName)
+	{
+		var recordingsRootPath = GetRecordingsRootPath(campaignName);
+		if (!Directory.Exists(recordingsRootPath))
+		{
+			return [];
+		}
+		return Directory.GetFiles(recordingsRootPath)
+				.Select(filePath => Path.GetFileNameWithoutExtension(filePath))
+				.ToArray();
+	}
+	public Stream? GetEpicMomentVideo(string campaignName, string recordingName)
+	{
+		var epicMomentVideoPath = GetEpicMomentVideoPath(campaignName, recordingName);
+		if (!File.Exists(epicMomentVideoPath))
+		{
+			return null;
+		}
+		var videoStream = File.OpenRead(epicMomentVideoPath);
+		return videoStream;
+	}
 	static string GetCharactersRootPath(string campaignName) =>
-		RecordingOperations.GetRecordingAssetsRootPath(campaignName, Constants.CharactersDirectoryName);
+		GetRecordingAssetsRootPath(campaignName, Constants.CharactersDirectoryName);
 	static string GetCharacterConfigFilePath(string campaignName) =>
 		Path.Combine(GetCharactersRootPath(campaignName), "list.json");
 	static string GetCharacterProfilePicturePath(string campaignName, string characterName)
@@ -101,12 +171,50 @@ public class CampaignStorageService
 
 		return Path.Combine(GetCharactersRootPath(campaignName), $"{characterName}.png");
 	}
-	public static string GetCampaignRootPath(string campaignName)
+	static string GetCampaignRootPath(string campaignName)
 	{
 		if (Path.IsPathRooted(campaignName) || campaignName.Contains("..", StringComparison.Ordinal))
 		{
 			throw new InvalidDataException("Name contains invalid characters.");
 		}
 		return Path.Combine(Constants.CampaignsDirectoryName, campaignName);
+	}
+	static string GetRecordingsRootPath(string campaignName)
+	{
+		return GetRecordingAssetsRootPath(campaignName, Constants.RecordingsDirectoryName);
+	}
+	static string GetTranscriptionsRootPath(string campaignName)
+	{
+		return GetRecordingAssetsRootPath(campaignName, Constants.TranscriptionDirectoryName);
+	}
+	static string GetEpicMomentsRootPath(string campaignName)
+	{
+		return GetRecordingAssetsRootPath(campaignName, Constants.EpicMomentsDirectoryName);
+	}
+	static string GetEpicMomentVideoPath(string campaignName, string recordingName) => Path.ChangeExtension(GetEpicMomentTextPath(campaignName, recordingName), ".mp4");
+	static string GetEpicMomentTextPath(string campaignName, string recordingName)
+	{
+		return Path.ChangeExtension(GetRecordingAssetPath(campaignName, recordingName, Constants.EpicMomentsDirectoryName), ".txt");
+	}
+	static string GetRecordingAssetsRootPath(string campaignName, string assetType)
+	{
+		ArgumentException.ThrowIfNullOrEmpty(assetType);
+		return Path.Combine(GetCampaignRootPath(campaignName), assetType);
+	}
+	static string GetRecordingAssetPath(string campaignName, string recordingName, string assetType)
+	{
+		if (Path.IsPathRooted(recordingName) || recordingName.Contains("..", StringComparison.Ordinal))
+		{
+			throw new InvalidDataException("Name contains invalid characters.");
+		}
+		return Path.Combine(GetRecordingAssetsRootPath(campaignName, assetType), recordingName);
+	}
+	static string GetRecordingPath(string campaignName, string recordingName)
+	{
+		return Path.ChangeExtension(GetRecordingAssetPath(campaignName, recordingName, Constants.RecordingsDirectoryName), ".mp3");
+	}
+	static string GetTranscriptionPath(string campaignName, string recordingName)
+	{
+		return Path.ChangeExtension(GetRecordingAssetPath(campaignName, recordingName, Constants.TranscriptionDirectoryName), ".txt");
 	}
 }
--- api/RecordingOperations.cs
+++ api/RecordingOperations.cs
@@ -5,47 +5,9 @@ namespace api;
 
 public static class RecordingOperations
 {
-	public static string GetRecordingsRootPath(string campaignName)
-	{
-		return GetRecordingAssetsRootPath(campaignName, Constants.RecordingsDirectoryName);
-	}
-	public static string GetTranscriptionsRootPath(string campaignName)
-	{
-		return GetRecordingAssetsRootPath(campaignName, Constants.TranscriptionDirectoryName);
-	}
-	public static string GetEpicMomentsRootPath(string campaignName)
-	{
-		return GetRecordingAssetsRootPath(campaignName, Constants.EpicMomentsDirectoryName);
-	}
-	public static string GetEpicMomentVideoPath(string campaignName, string recordingName) => Path.ChangeExtension(GetEpicMomentTextPath(campaignName, recordingName), ".mp4");
-	public static string GetEpicMomentTextPath(string campaignName, string recordingName)
-	{
-		return Path.ChangeExtension(GetRecordingAssetPath(campaignName, recordingName, Constants.EpicMomentsDirectoryName), ".txt");
-	}
-	public static string GetRecordingAssetsRootPath(string campaignName, string assetType)
-	{
-		ArgumentException.ThrowIfNullOrEmpty(assetType);
-		return Path.Combine(CampaignStorageService.GetCampaignRootPath(campaignName), assetType);
-	}
-	public static string GetRecordingAssetPath(string campaignName, string recordingName, string assetType)
-	{
-		if (Path.IsPathRooted(recordingName) || recordingName.Contains("..", StringComparison.Ordinal))
-		{
-			throw new InvalidDataException("Name contains invalid characters.");
-		}
-		return Path.Combine(GetRecordingAssetsRootPath(campaignName, assetType), recordingName);
-	}
-	public static string GetRecordingPath(string campaignName, string recordingName)
-	{
-		return Path.ChangeExtension(GetRecordingAssetPath(campaignName, recordingName, Constants.RecordingsDirectoryName), ".mp3");
-	}
-	public static string GetTranscriptionPath(string campaignName, string recordingName)
-	{
-		return Path.ChangeExtension(GetRecordingAssetPath(campaignName, recordingName, Constants.TranscriptionDirectoryName), ".txt");
-	}
 	public static void AddRecordingOperations(this WebApplication app)
 	{
-		app.MapPost($"{Constants.CampaignsApiSegment}/{{campaignName}}{Constants.RecordingsApiSegment}", async (HttpRequest request, string campaignName, AudioClient client, CancellationToken cancellationToken) =>
+		app.MapPost($"{Constants.CampaignsApiSegment}/{{campaignName}}{Constants.RecordingsApiSegment}", async (HttpRequest request, string campaignName, CampaignStorageService storageService, AudioClient client, CancellationToken cancellationToken) =>
 		{
 			// Set max request body size to 1 GB for this endpoint
 			var maxRequestBodySizeFeature = request.HttpContext.Features.Get<Microsoft.AspNetCore.Http.Features.IHttpMaxRequestBodySizeFeature>();
@@ -70,61 +32,38 @@ public static class RecordingOperations
 				TimestampGranularities = AudioTimestampGranularities.Word | AudioTimestampGranularities.Segment,
 			};
 
-			var results = new List<object>();
+			var results = new List<string>();
 			foreach (var file in form.Files)
 			{
-				// Process each uploaded file here
-				// For example, you can save the file to a specific location
-				var recordingsRootPath = GetRecordingsRootPath(campaignName);
-				if (!Directory.Exists(recordingsRootPath))
-				{
-					Directory.CreateDirectory(recordingsRootPath);
-				}
-				var filePath = Path.Combine(recordingsRootPath, file.FileName);
-
-				using var fileStream = File.Create(filePath);
 				using var ms = new MemoryStream();
 				await file.CopyToAsync(ms, cancellationToken).ConfigureAwait(false);
 				ms.Position = 0;
-				await ms.CopyToAsync(fileStream, cancellationToken).ConfigureAwait(false);
+				var fileName = await storageService.SaveRecordingAsync(campaignName, file.FileName, ms, cancellationToken).ConfigureAwait(false);
 				ms.Position = 0;
 
-				var fileName = Path.GetFileName(filePath);
 				var transcription = await AudioHelper.ChunkAndMergeTranscriptsIfRequired(ms, fileName, options, client, cancellationToken).ConfigureAwait(false);
-				var transcriptionsRootPath = GetTranscriptionsRootPath(campaignName);
-				if (!Directory.Exists(transcriptionsRootPath))
-				{
-					Directory.CreateDirectory(transcriptionsRootPath);
-				}
-				var transcriptionPath = Path.Combine(transcriptionsRootPath, Path.ChangeExtension(fileName, ".txt"));
-				await File.WriteAllTextAsync(transcriptionPath, transcription, cancellationToken).ConfigureAwait(false);
-				results.Add(new { file = filePath, transcriptionFile = transcriptionPath });
+				await storageService.SaveTranscriptionAsync(campaignName, fileName, transcription, cancellationToken).ConfigureAwait(false);
+				results.Add(Path.GetFileNameWithoutExtension(fileName));
 			}
 
 			return Results.Ok(results);
 		}).WithName("UploadRecording").WithOpenApi().DisableRequestTimeout();
 
-		app.MapGet($"{Constants.CampaignsApiSegment}/{{campaignName}}{Constants.RecordingsApiSegment}", (string campaignName) =>
+		app.MapGet($"{Constants.CampaignsApiSegment}/{{campaignName}}{Constants.RecordingsApiSegment}", (string campaignName, CampaignStorageService storageService) =>
 		{
-			var files = Directory.GetFiles(GetRecordingsRootPath(campaignName))
-				.Select(filePath => new
-				{
-					FileName = Path.GetFileName(filePath),
-					Url = $"{Constants.RecordingsApiSegment}/{Path.GetFileName(filePath)}"
-				})
-				.ToList();
+			var files = storageService.GetRecordings(campaignName)
+				.ToArray();
 
 			return Results.Ok(files);
 		}).WithName("ListRecordings").WithOpenApi();
 
-		app.MapPost($"{Constants.CampaignsApiSegment}/{{campaignName}}{Constants.RecordingsApiSegment}/{{recordingName}}{Constants.EpicMomentsApiSegment}", async (string campaignName, string recordingName, CustomVideoClient customVideoClient, ChatClient client, IHttpClientFactory httpClientFactory, CancellationToken cancellationToken) =>
+		app.MapPost($"{Constants.CampaignsApiSegment}/{{campaignName}}{Constants.RecordingsApiSegment}/{{recordingName}}{Constants.EpicMomentsApiSegment}", async (string campaignName, string recordingName, CampaignStorageService storageService, CustomVideoClient customVideoClient, ChatClient client, IHttpClientFactory httpClientFactory, CancellationToken cancellationToken) =>
 		{
-			var transcriptionFile = GetTranscriptionPath(campaignName, recordingName);
-			if (!File.Exists(transcriptionFile))
+			var transcription = await storageService.GetTranscriptionAsync(campaignName, recordingName, cancellationToken);
+			if (string.IsNullOrEmpty(transcription))
 			{
 				return Results.NotFound("Transcription not found.");
 			}
-			var transcript = await File.ReadAllTextAsync(transcriptionFile, cancellationToken).ConfigureAwait(false);
 			var result = await client.CompleteChatAsync(
 			[
 				new SystemChatMessage(
@@ -133,36 +72,27 @@ public static class RecordingOperations
 					Analyze the transcript of this play session and extract a tale of an epic encounter. You might recount it as a 10 sentences story or ballad.
 					Exaggerate the details and the facts to make it more interesting and entertaining.
 					"""),
-				new UserChatMessage(transcript)
+				new UserChatMessage(transcription)
 			], cancellationToken: cancellationToken).ConfigureAwait(false);
 			var taleContent = result.Value.Content[0].Text;
-			var taleFile = GetEpicMomentTextPath(campaignName, recordingName);
-			var taleDirectory = GetEpicMomentsRootPath(campaignName);
-			if (!Directory.Exists(taleDirectory) && taleDirectory is not null)
-			{
-				Directory.CreateDirectory(taleDirectory);
-			}
-			await File.WriteAllTextAsync(taleFile, taleContent, cancellationToken).ConfigureAwait(false);
+			await storageService.SaveEpicMomentTaleAsync(campaignName, recordingName, taleContent, cancellationToken).ConfigureAwait(false);
 			using var epicMomentVideo = await customVideoClient.GetEpicMomentVideoAsync(taleContent, cancellationToken).ConfigureAwait(false);
 			if (epicMomentVideo is null)
 			{
 				return Results.StatusCode(500);
 			}
-			var epicMomentVideoPath = GetEpicMomentVideoPath(campaignName, recordingName);
-			using var videoFile = File.Create(epicMomentVideoPath);
-			await epicMomentVideo.CopyToAsync(videoFile, cancellationToken).ConfigureAwait(false);
+			await storageService.SaveEpicMomentVideoAsync(campaignName, recordingName, epicMomentVideo, cancellationToken).ConfigureAwait(false);
 			return Results.Created();
 		}).WithName("CreateEpicMoment").WithOpenApi();
 
-		app.MapGet($"{Constants.CampaignsApiSegment}/{{campaignName}}{Constants.RecordingsApiSegment}/{{recordingName}}{Constants.EpicMomentsApiSegment}", (string campaignName, string recordingName) =>
+		app.MapGet($"{Constants.CampaignsApiSegment}/{{campaignName}}{Constants.RecordingsApiSegment}/{{recordingName}}{Constants.EpicMomentsApiSegment}", (string campaignName, string recordingName, CampaignStorageService storageService) =>
 		{
-			var epicMomentVideoPath = GetEpicMomentVideoPath(campaignName, recordingName);
-			if (!File.Exists(epicMomentVideoPath))
+			var epicMomentVideoStream = storageService.GetEpicMomentVideo(campaignName, recordingName);
+			if (epicMomentVideoStream is null)
 			{
 				return Results.NotFound("Epic moment video not found.");
 			}
-			var videoStream = File.OpenRead(epicMomentVideoPath);
-			return Results.File(videoStream, "video/mp4", Path.GetFileName(epicMomentVideoPath));
+			return Results.Stream(epicMomentVideoStream, "video/mp4");
 		}).WithName("GetEpicMoment").WithOpenApi();
 	}
 }
